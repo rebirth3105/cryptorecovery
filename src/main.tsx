2a907c77a6577a1a4df53ff089e11eaa4cf505e0
Rebirth3105 <aabimbola484@gmail.com>
2026-03-08 04:13:59 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,5 @@ import { createRoot } from 'react-dom/client'
 import './main.css'
 import App from './App'
 
-const root = createRoot(document.getElementById('app')!)
+const root = createRoot(document.getElementById('root')!)
 root.render(<App />)
